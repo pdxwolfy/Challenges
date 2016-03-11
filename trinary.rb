4d17fc6dd23e43bc2418624e1aefbf1931bc56df
Pete Hanson <wolfy@pdxwolfy.org>
2016-03-11 14:07:09 -0800

    Whitespace elimination

--- trinary.rb
+++ trinary.rb
@@ -5,7 +5,7 @@
 class Trinary
   # Problem statement is such that we can't use String#to_i. However, even if
   # it were allowed, using this hash makes error detection easier.
-  TO_I = { '0' => 0, '1' => 1, '2' => 2 }.freeze 
+  TO_I = { '0' => 0, '1' => 1, '2' => 2 }.freeze
 
   def initialize(trinary_number_string)
     @trinary_number = trinary_number_string
