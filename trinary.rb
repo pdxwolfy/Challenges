38c22ac65f21883d2e8c277b415f6dda5477196f
Pete Hanson <wolfy@pdxwolfy.org>
2016-03-11 13:59:06 -0800

    Use #each_char instead of #chars

--- trinary.rb
+++ trinary.rb
@@ -6,7 +6,7 @@ class Trinary
   end
 
   def to_decimal
-    @trinary_number.chars.inject(0) { |sum, digit| 3 * sum + TO_I[digit] }
+    @trinary_number.each_char.inject(0) { |sum, digit| 3 * sum + TO_I[digit] }
   rescue TypeError
     0
   end
