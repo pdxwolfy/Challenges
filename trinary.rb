3c0716ead41679b75d0616591860e37dcc65e8b5
Pete Hanson <wolfy@pdxwolfy.org>
2016-03-10 18:08:19 -0800

    Variable rename

--- trinary.rb
+++ trinary.rb
@@ -1,10 +1,10 @@
 class Trinary
   def initialize(trinary_number_string)
-    @trinary = trinary_number_string
+    @trinary_number = trinary_number_string
   end
 
   def to_decimal
-    return 0 unless @trinary =~ /\A[012]+\Z/
-    @trinary.chars.inject(0) { |memo, digit| 3 * memo + digit.to_i }
+    return 0 unless @trinary_number =~ /\A[012]+\Z/
+    @trinary_number.chars.inject(0) { |memo, digit| 3 * memo + digit.to_i }
   end
 end
