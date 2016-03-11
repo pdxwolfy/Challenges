c8d3e236538061670838fc4cd30d606d6b5fe6df
Pete Hanson <wolfy@pdxwolfy.org>
2016-03-10 18:46:09 -0800

    Eliminated use of to_i - may be contrary to stated conditions.

--- trinary.rb
+++ trinary.rb
@@ -1,10 +1,12 @@
 class Trinary
+  TO_I = { '0' => 0, '1' => 1, '2' => 2 } # Can't use to_i
+
   def initialize(trinary_number_string)
     @trinary_number = trinary_number_string
   end
 
   def to_decimal
     return 0 unless @trinary_number =~ /\A[012]+\Z/
-    @trinary_number.chars.inject(0) { |memo, digit| 3 * memo + digit.to_i }
+    @trinary_number.chars.inject(0) { |memo, digit| 3 * memo + TO_I[digit] }
   end
 end
